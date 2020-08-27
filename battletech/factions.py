c69943574e637f6048ea39568e0f77e584ccb550
inimitable <inimitable@users.noreply.github.com>
2020-08-27 13:58:02 -0400

    Updating factions

--- battletech/factions.py
+++ battletech/factions.py
@@ -1,4 +1,5 @@
 from toml import load
+from dataclasses import dataclass
 from pathlib import Path
 
 with open(Path(__file__).parent / 'values.toml') as f:
@@ -6,6 +7,39 @@ with open(Path(__file__).parent / 'values.toml') as f:
 
 NAMES_BY_FACTION = {v: k for k, v in FACTIONS_BY_NAME.items()}
 FACTIONS = [v for _, v in FACTIONS_BY_NAME.items()]
+EMPLOYERS = ["Davion", "Kurita", "Liao", "AuriganPirates", "Steiner", "Marik", "ComStar", "AuriganRestoration"]
+
+
+@dataclass
+class Faction:
+    id: str
+    has_rep: bool = True
+    rep_matters: bool = True
+
+
+    @property
+    def is_target(self):
+        return not (self.has_rep or not self.rep_matters)
+
+    def is_employer(self):
+        return self.id in EMPLOYERS
+
+
+# Here comes the tedious part!
+# {'Locals', 'Liao', 'Kurita', 'TaurianConcordat', 'Davion', 'MagistracyOfCanopus', 'Steiner', 'Marik', 'AuriganPirates', 'ComStar', 'AuriganRestoration'}
+FEDERATED_SUNS = FEDSUNS = DAVION = 'Davion'
+CAPELLAN_CONFEDERATION = CAPELLANS = LIAO = "Liao"
+DRACONIS_COMBINE = KURITANS = KURITA = "Kurita"
+MAGISTRACY_OF_CANOPUS = CANOPIANS = CANOPUS = "MagistracyOfCanopus"
+LYRAN_COMMONWEALTH = LYRANS = STEINER = "Steiner"
+FREE_WORLDS_LEAGUE = FREE_WORLDS = MARIK = "Marik"
+AURIGAN_PIRATES = PIRATES = OUTLAWS = "AuriganPirates"
+SHADY_SPACE_WIZARD_ILLUMINATI = COMSTAR = "Comstar"
+AURIGAN_RESTORATION = AURIGAN = KEONA = "AuriganRestoration"
+
+PLANETARY_GOVERNMENT = 'Locals'
+TAURIANS = 'TaurianConcordat'
+
 
 PREFERRED_EMPLOYERS = ['MagistracyOfCanopus', 'Davion', 'Marik', 'ComStar', 'Steiner']
 PREFERRED_ENEMIES = ['Liao', 'ComStar', 'Kurita']
